9ff011018c440b73ffa1117ad9259dc6e282ecc0
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2022-11-20 23:19:37 +0000

    wip
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- aws/op_iam_Role.go
+++ aws/op_iam_Role.go
@@ -1,20 +1,20 @@
 package aws
 
 import (
-	"context"
-	"fmt"
+  "context"
+  "fmt"
 
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	_ "github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go-v2/service/iam"
+  _ "github.com/aws/aws-sdk-go-v2/service/sts"
+  "github.com/aws/aws-sdk-go/aws"
 
-	"awshowto/internal"
+  "awshowto/internal"
 )
 
 const (
-	ExampleRoleName   = "ik-golang-example-role"
-	ExamplePolicyARN  = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
-	ExampleSLRService = "elasticbeanstalk.amazonaws.com"
+  ExampleRoleName   = "ik-golang-example-role"
+  ExamplePolicyARN  = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+  ExampleSLRService = "elasticbeanstalk.amazonaws.com"
 )
 
 var (
@@ -31,20 +31,20 @@ var (
 )
 
 func (cmp Compute) CreateRole() {
-	service := iam.NewFromConfig(cmp.Config)
-	getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
-		RoleName: aws.String(ExampleRoleName),
-	})
-	if getRole != nil {
-		internal.ExitErrorf(fmt.Sprintf("Role found!!!. role arn: %s.", *getRole.Role.Arn))
-	}
-	if err != nil {
-		// snippet-start:[iam.go-v2.CreateRole]
-		// CreateRole
-		role, err := service.CreateRole(context.Background(), &iam.CreateRoleInput{
-			RoleName:    aws.String(ExampleRoleName),
-			Description: aws.String("My super awesome example role"),
-			AssumeRolePolicyDocument: aws.String(`{
+  service := iam.NewFromConfig(cmp.Config)
+  getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
+    RoleName: aws.String(ExampleRoleName),
+  })
+  if getRole != nil {
+    internal.ExitErrorf(fmt.Sprintf("Role found!!!. role arn: %s.", *getRole.Role.Arn))
+  }
+  if err != nil {
+    // snippet-start:[iam.go-v2.CreateRole]
+    // CreateRole
+    role, err := service.CreateRole(context.Background(), &iam.CreateRoleInput{
+      RoleName:    aws.String(ExampleRoleName),
+      Description: aws.String("My super awesome example role"),
+      AssumeRolePolicyDocument: aws.String(`{
         "Version": "2012-10-17",
         "Statement": [
         {
@@ -57,73 +57,74 @@ func (cmp Compute) CreateRole() {
           }
         ]
       }`),
-		})
-		if err != nil {
-			internal.CheckError("Couldn't create role.", err)
-		}
-		internal.OutputColorizedMessage("green", "✅ Role Created")
-		internal.OutputColorizedMessage("green", fmt.Sprintf("Account:: %s.", *role.Role.Arn))
-		// snippet-end:[iam.go-v2.CreateRole]
-	}
+    })
+    if err != nil {
+      internal.CheckError("Couldn't create role.", err)
+    }
+    internal.OutputColorizedMessage("green", "✅ Role Created")
+    internal.OutputColorizedMessage("green", fmt.Sprintf("Account:: %s.", *role.Role.Arn))
+    // snippet-end:[iam.go-v2.CreateRole]
+  }
 }
 
 func (cmp Compute) AttachInlinePolicies() {
-	service := iam.NewFromConfig(cmp.Config)
-	getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
-		RoleName: aws.String(ExampleRoleName),
-	})
-	if err != nil {
-		internal.CheckError(fmt.Sprintf("Role (%s) not found.", ExampleRoleName), err)
-	}
-	if getRole != nil {
+  service := iam.NewFromConfig(cmp.Config)
+  getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
+    RoleName: aws.String(ExampleRoleName),
+  })
+  if err != nil {
+    internal.CheckError(fmt.Sprintf("Role (%s) not found.", ExampleRoleName), err)
+  }
+  
+  if getRole != nil {
     for _, policy := range ExampleInlinePoliciesV1List {
       _, err = service.PutRolePolicy(context.Background(), &iam.PutRolePolicyInput{
-        RoleName: aws.String(ExampleRoleName),
-        PolicyName: aws.String(policy.Key),
+        RoleName:       aws.String(ExampleRoleName),
+        PolicyName:     aws.String(policy.Key),
         PolicyDocument: aws.String(policy.Value),
       })
       if err != nil {
         internal.CheckError(fmt.Sprintf("Role (%s) inline policy not attached.", ExampleRoleName), err)
       }
     }
-		internal.OutputColorizedMessage("green", fmt.Sprintf("Inline policies attached to role (%s).", ExampleRoleName))
-	}
+    internal.OutputColorizedMessage("green", fmt.Sprintf("Inline policies attached to role (%s).", ExampleRoleName))
+  }
 }
 
 func (cmp Compute) DeleteRole() {
-	service := iam.NewFromConfig(cmp.Config)
-	getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
-		RoleName: aws.String(ExampleRoleName),
-	})
-	if err != nil {
-		internal.CheckError(fmt.Sprintf("Role (%s) not found.", ExampleRoleName), err)
-	}
-	if getRole != nil {
-		_, err = service.DeleteRole(context.Background(), &iam.DeleteRoleInput{
-			RoleName: aws.String(ExampleRoleName),
-		})
-		internal.CheckError(fmt.Sprintf("FAILED > Role (%s) deletion.", ExampleRoleName), err)
-		internal.OutputColorizedMessage("green", fmt.Sprintf("✅ SUCCESS > Role (%s) deleted.", ExampleRoleName))
-	}
+  service := iam.NewFromConfig(cmp.Config)
+  getRole, err := service.GetRole(context.Background(), &iam.GetRoleInput{
+    RoleName: aws.String(ExampleRoleName),
+  })
+  if err != nil {
+    internal.CheckError(fmt.Sprintf("Role (%s) not found.", ExampleRoleName), err)
+  }
+  if getRole != nil {
+    _, err = service.DeleteRole(context.Background(), &iam.DeleteRoleInput{
+      RoleName: aws.String(ExampleRoleName),
+    })
+    internal.CheckError(fmt.Sprintf("FAILED > Role (%s) deletion.", ExampleRoleName), err)
+    internal.OutputColorizedMessage("green", fmt.Sprintf("✅ SUCCESS > Role (%s) deleted.", ExampleRoleName))
+  }
 }
 
 func (cmp Compute) ListRoles() {
-	service := iam.NewFromConfig(cmp.Config)
+  service := iam.NewFromConfig(cmp.Config)
 
-	roles, err := service.ListRoles(context.Background(), &iam.ListRolesInput{})
+  roles, err := service.ListRoles(context.Background(), &iam.ListRolesInput{})
 
-	if err != nil {
-		internal.CheckError("Could not list roles.", err)
-	}
-	fmt.Println("🔰 List Roles")
-	for _, idxRole := range roles.Roles {
-		fmt.Printf("ID: %s. NAME: %s. ARN: %s.",
-			*idxRole.RoleId,
-			*idxRole.RoleName,
-			*idxRole.Arn)
-		if idxRole.Description != nil {
-			fmt.Printf("DESC: %s", *idxRole.Description)
-		}
-		fmt.Print("\n")
-	}
+  if err != nil {
+    internal.CheckError("Could not list roles.", err)
+  }
+  fmt.Println("🔰 List Roles")
+  for _, idxRole := range roles.Roles {
+    fmt.Printf("ID: %s. NAME: %s. ARN: %s.",
+      *idxRole.RoleId,
+      *idxRole.RoleName,
+      *idxRole.Arn)
+    if idxRole.Description != nil {
+      fmt.Printf("DESC: %s", *idxRole.Description)
+    }
+    fmt.Print("\n")
+  }
 }
